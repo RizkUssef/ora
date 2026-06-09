ae65e468cf5e260d411913128f1a90c685dbb3d2
RizkUssef <rizkusgt67@gmail.com>
2026-06-10 02:09:35 +0300

    add: add title and lines in the StatsBanner section

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+'use client';
 import PrimaryButton from "../ui/PrimaryButton";
 import SecondaryButton from "../ui/SecondaryButton";
 
--- components/home/StatsBanner.tsx
+++ components/home/StatsBanner.tsx
@@ -1,15 +1,39 @@
-'use client';
-import StatsCard from '../common/StatsCard'
+"use client";
+import StatsCard from "../common/StatsCard";
 
 const StatsBanner = () => {
   return (
     <div className="stats-banner container">
-      <StatsCard icon={<span>🎉</span>} label="500" span="+" description="Events hosted" />
-      <StatsCard icon={<span>🏛️</span>} label="50" span="yrs" description="Heritage" />
-      <StatsCard icon={<span>👥</span>} label="1,200" description="Max capacity" />
-      <StatsCard icon={<span>⭐</span>} label="4.9" span="/5" description="Guest rating" />
+      <div className="stats-banner-heading">
+        <p className="">Hall at a glance</p>
+      </div>
+      <div className="flex justify-evenly items-center w-full">
+        <StatsCard
+          icon={<span>🎉</span>}
+          label="500"
+          span="+"
+          description="Events hosted"
+        />
+        <StatsCard
+          icon={<span>🏛️</span>}
+          label="50"
+          span="yrs"
+          description="Heritage"
+        />
+        <StatsCard
+          icon={<span>👥</span>}
+          label="1,200"
+          description="Max capacity"
+        />
+        <StatsCard
+          icon={<span>⭐</span>}
+          label="4.9"
+          span="/5"
+          description="Guest rating"
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default StatsBanner
+export default StatsBanner;
